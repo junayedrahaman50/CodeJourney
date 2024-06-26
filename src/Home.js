b7d321f28a50b8d57c3c4258364a48a4973eb594
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2024-06-27 01:27:20 +0530

    coding challenge tracker app

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1 className="heading-primary">Junayed's 100 Days of Code Journey</h1>
+        <button className="btn-primary mt-md">Explore All</button>
+      </div>
+      <div className="content">
+        <div className="card mt-lg">
+          <h2 style={{ fontSize: "2rem", fontWeight: "var(--SEMI-BOLD)" }}>
+            Day 1
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
