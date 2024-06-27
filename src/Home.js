48ee1b18f20673f40a16919c6c543a0f5a4ba96b
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2024-06-27 13:48:05 +0530

    added tweet embed

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+import { TwitterTweetEmbed } from "react-twitter-embed";
+
 const Home = () => {
   return (
     <div className="container">
@@ -7,9 +9,14 @@ const Home = () => {
       </div>
       <div className="content">
         <div className="card mt-lg">
-          <h2 style={{ fontSize: "2rem", fontWeight: "var(--SEMI-BOLD)" }}>
+          {/* <h2 style={{ fontSize: "2rem", fontWeight: "var(--SEMI-BOLD)" }}>
             Day 1
           </h2>
+          <p style={{ fontSize: "var(--font-size-medium)" }}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident,
+            laboriosam.
+          </p> */}
+          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
         </div>
       </div>
     </div>
