cffaedfaccb02e50275ee3dfbebeeb89088bbcd8
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2024-06-27 22:37:42 +0530

    Added new styles

--- src/Home.js
+++ src/Home.js
@@ -1,52 +1,52 @@
-import { TwitterTweetEmbed } from "react-twitter-embed";
-
+// import { TwitterTweetEmbed } from "react-twitter-embed";
+import { Twitter } from "lucide-react";
 const Home = () => {
   return (
     <div className="container">
       <div className="header">
         <h1 className="heading-primary">Junayed's 100 Days of Code Journey</h1>
-        <button className="btn-primary mt-md">Explore All</button>
+        <button className="btn-primary mt-md">
+          Follow my journey on
+          <Twitter />
+        </button>
       </div>
       <div className="content">
         <div className="card mt-lg">
-          {/* <h2 style={{ fontSize: "2rem", fontWeight: "var(--SEMI-BOLD)" }}>
+          <h2 style={{ fontSize: "2rem", fontWeight: "var(--SEMI-BOLD)" }}>
             Day 1
           </h2>
           <p style={{ fontSize: "var(--font-size-medium)" }}>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident,
             laboriosam.
-          </p> */}
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
+          </p>
+          {/* <TwitterTweetEmbed tweetId={"1805659724018598152"} /> */}
         </div>
         <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
+          <h2 style={{ fontSize: "2rem", fontWeight: "var(--SEMI-BOLD)" }}>
+            Day 1
+          </h2>
+          <p style={{ fontSize: "var(--font-size-medium)" }}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident,
+            laboriosam.
+          </p>
         </div>
-        <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
+        <div className="card mt-md">
+          <h2 style={{ fontSize: "2rem", fontWeight: "var(--SEMI-BOLD)" }}>
+            Day 1
+          </h2>
+          <p style={{ fontSize: "var(--font-size-medium)" }}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident,
+            laboriosam.
+          </p>
         </div>
-        <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
-        </div>
-        <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
-        </div>
-        <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
-        </div>
-        <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
-        </div>
-        <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
-        </div>
-        <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
-        </div>
-        <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
-        </div>
-        <div className="card mt-lg">
-          <TwitterTweetEmbed tweetId={"1805659724018598152"} />
+        <div className="card mt-md">
+          <h2 style={{ fontSize: "2rem", fontWeight: "var(--SEMI-BOLD)" }}>
+            Day 1
+          </h2>
+          <p style={{ fontSize: "var(--font-size-medium)" }}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident,
+            laboriosam.
+          </p>
         </div>
       </div>
     </div>
