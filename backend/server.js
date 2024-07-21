1ef2478fe5ce6b5cee597a0bbcf73672bb63ea15
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2024-07-21 14:12:18 +0530

    made separate backend and frontend directory

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// global middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the app!" }); // send back a json string
+});
+
+// attach all routes specified in router to the app, on getting request to the specified path use the routes
+// app.use("/api/posts", postRoutes);
+
+// Connect to database:
+// connect method is asynchronus it returns a promise, then method works when the promise is fulfilled
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests when connected to database
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to DB & listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(err.message));
