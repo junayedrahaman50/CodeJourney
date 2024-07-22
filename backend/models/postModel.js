a915fbd6275a5c0732273488d11b3139fc559511
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2024-07-22 18:26:02 +0530

    changes in post model

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -1,44 +1,50 @@
 const mongoose = require("mongoose");
 
-// Define the notes schema
-const noteSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
+// Define the notes schema with timestamps
+const noteSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+); // Enable timestamps
 
-// Define the main schema
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+// Define the main schema with timestamps
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: [noteSchema], // An array of noteSchema
+      default: [], // Default to an empty array if not provided
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  notes: {
-    type: [noteSchema], // An array of noteSchema
-    default: [], // Default to an empty array if not provided
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+); // Enable timestamps
 
 // Create the model
 module.exports = mongoose.model("Post", postSchema);
